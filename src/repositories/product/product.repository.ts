746d3ac61eb3694b818b958b9ad99ff67a270a87
David Ambrozio <davidambroziodev@gmail.com>
2024-05-03 14:52:08 -0300

    update product repository interface and create product service interface

--- src/repositories/product/product.repository.ts
+++ src/repositories/product/product.repository.ts
@@ -3,4 +3,6 @@ import { Product } from "../../entity/products";
 export interface ProductRepository {
     save(product: Product): Promise<void>;
     list(): Promise<Product[]>;
+    update(product: Product): Promise<void>;
+    find(id:string): Promise<Product>;
 }
---
+++ src/services/product/product.service.ts
@@ -0,0 +1,25 @@
+export type SellOutputDto = {
+    id: string;
+    balance: string;
+}
+
+export type BuyOutputDto = {
+    id: string;
+    balance: string;
+}
+
+export type ListOutputDto = {
+    products: {
+        id: string;
+        name: string;
+        price: number;
+        balance: number;
+    }
+    
+}
+
+export interface ProductService {
+    sell(id: string, amount: number): Promise<SellOutputDto>;
+    buy(id: string, amount: number): Promise<BuyOutputDto>;
+    list(): Promise<ListOutputDto>;
+}
